6ef136e996dfb5011d37f9e74c1354dba28ffc34
Till Bauer <bauertill@gmail.com>
2022-04-27 18:48:25 +0200

    Fix uniform type detection

--- data_class.py
+++ data_class.py
@@ -1,4 +1,5 @@
-from typing import Dict, Any
+from collections import Counter
+from typing import Dict, Any, List
 
 import pandas as pd
 from pandas.core.dtypes.common import is_numeric_dtype
@@ -9,35 +10,43 @@ from pandas.core.dtypes.common import is_numeric_dtype
 # - **MISSING VALUES** Are there any null / missing values?
 # - **OUTLIERS** Any outliers in the data (per column)?
 
+def _determine_best_type(value: Any):
+    try:
+        float(value)
+        return "numeric"
+    except ValueError:
+        pass
+    return "string"
+
 
 class DataClass:
     def __init__(self, path: str) -> None:
         self.df: pd.DataFrame = pd.read_csv(path)
 
     def check_uniformity(self) -> Dict[str, Any]:
-        uniformity_report = {}
+        bad_types_by_column = {}
         for column_index in self.df:
             column = self.df[column_index]
-            types_in_column = set(type(value) for value in column)
-            if len(types_in_column) > 1:
-                uniformity_report[
-                    str(column.name)
-                ] = "contains different types " + " ,".join(str(types_in_column))
-        return uniformity_report
+            counter = Counter([_determine_best_type(value=value) for value in column])
+            if len(counter) > 1:
+                most_common_type = counter.most_common()
+                non_confirming_indexes = [i  for i, value in enumerate(column) if _determine_best_type(value=value) ==most_common_type ]
+                bad_types_by_column[str(column.name)] = non_confirming_indexes
+        return bad_types_by_column
 
-    def check_duplicates(self):
+    def check_duplicates(self) -> List[int]:
         df = self.df[self.df.duplicated(keep=False)]
         duplicate_rows = df.groupby(list(df)).apply(lambda x: tuple(x.index))
         if not duplicate_rows.empty:
             return duplicate_rows.tolist()
         return []
 
-    def check_missing_values(self):
+    def check_missing_values(self)-> List[int]:
         null_data = self.df[self.df.isnull().any(axis=1)]
         return null_data.index.tolist()
 
-    def detect_outliers(self):
-        # For simplicity outliers means 3 standard deviations from the mean
+    def detect_outliers(self)-> Dict[str, Any]:
+        # For simplicity outliers are defined as 3 standard deviations from the mean
         outliers_by_column = {}
         for column_index in self.df:
             column = self.df[column_index]
@@ -54,3 +63,4 @@ class DataClass:
             "OUTLIERS": self.detect_outliers()
         }
         return report
+
