9df3b2ff8264da9f5f3abfc3cca533b71c989d8a
Till Bauer <bauertill@gmail.com>
2022-04-28 09:52:28 +0200

    Add command line arguments

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from pprint import pprint
 from data_class import DataClass
+import argparse
 
-# Hardcoded for now, could be changed to a command line argument
 CSV_PATH = "data/data.csv"
 
 
 def main():
-    data_class = DataClass(CSV_PATH)
+    parser = argparse.ArgumentParser(description="Simple tool for checking csv files.")
+    parser.add_argument(
+        "--path", type=str, help="the path to the csv file", required=True
+    )
+    args = parser.parse_args()
+    data_class = DataClass(args.path)
     report = data_class.generate_report()
     pprint(report)
 
